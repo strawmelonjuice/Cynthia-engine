f38155db60dc80b26bcad39f2024c22f5a6f4893
Mar <mar@strawmelonjuice.com>
2024-02-21 23:37:10 +0100

    sum formatting

--- src/dashfunctions.rs
+++ src/dashfunctions.rs
@@ -2,10 +2,10 @@ use normalize_path::NormalizePath;
 use random_string::generate_rng;
 use std::fs;
 use std::fs::File;
-use std::io::{Error, Write};
+use std::io::{ Error, Write };
 use std::path::PathBuf;
 use std::sync::Mutex;
-use actix_web::{post, HttpResponse, web};
+use actix_web::{ post, HttpResponse, web };
 use actix_web::web::Data;
 use serde::Deserialize;
 use crate::logger::logger;
@@ -19,15 +19,28 @@ struct DashAPIData {
     params: String,
 }
 #[post("/dashapi/")]
-pub(crate) async fn dashserver(data:  web::Form<DashAPIData>, _pluginsmex: Data<Mutex<Vec<PluginMeta>>>) -> HttpResponse {
-    if data.passkey != (passkey().unwrap_or(String::from(""))) {
-        logger(15, String::from("An unauthorized external entity just tried performing an action on this instance."));
-        return HttpResponse::Forbidden().body(String::from("Wrong passkey entered. A report has been sent to the server owner."));
+pub(crate) async fn dashserver(
+    data: web::Form<DashAPIData>,
+    _pluginsmex: Data<Mutex<Vec<PluginMeta>>>
+) -> HttpResponse {
+    if data.passkey != passkey().unwrap_or(String::from("")) {
+        logger(
+            15,
+            String::from(
+                "An unauthorized external entity just tried performing an action on this instance."
+            )
+        );
+        return HttpResponse::Forbidden().body(
+            String::from("Wrong passkey entered. A report has been sent to the server owner.")
+        );
     }
 
     match data.command.as_str() {
         "log" => {
-            logger(data.subcommand.parse().unwrap_or(1), format!("{{CynthiaDash}} {}",data.params.clone()));
+            logger(
+                data.subcommand.parse().unwrap_or(1),
+                format!("{{CynthiaDash}} {}", data.params.clone())
+            );
         }
         _ => {
             return HttpResponse::BadRequest().body(String::from("Invalid command."));
@@ -35,7 +48,6 @@ pub(crate) async fn dashserver(data:  web::Form<DashAPIData>, _pluginsmex: Data<
     }
 
     HttpResponse::Ok().body(String::from("OK!"))
-
 }
 
 pub fn passkey() -> Result<String, Error> {
