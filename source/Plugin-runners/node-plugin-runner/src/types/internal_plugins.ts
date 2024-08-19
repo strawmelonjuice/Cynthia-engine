ff3fdcd9c1e6a769f5fd10894580028e8b2ab4ac
Mar <mar@strawmelonjuice.com>
2024-08-19 14:46:18 +0200

    CHORE: Solve Biome warnings

--- source/Plugin-runners/node-plugin-runner/src/types/internal_plugins.ts
+++ source/Plugin-runners/node-plugin-runner/src/types/internal_plugins.ts
@@ -9,35 +9,39 @@ import {
   IncomingWebRequest,
   type ResponderResponse,
 } from "cynthia-plugin-api/main";
-import {ContentMetaDataType, CynthiaApiPoints} from "../../../node-plugin-api/main";
+import type {
+  ContentMetaDataType,
+  CynthiaApiPoints,
+} from "../../../node-plugin-api/main";
 export const Plugincompat = 3.2;
 export interface PluginBase {
   modifyResponseHTML: Array<
-      (htmlin: string,
-       metadata: ContentMetaDataType,
-       Cynthia: CynthiaApiPoints) => string
+    (
+      htmlin: string,
+      metadata: ContentMetaDataType,
+      Cynthia: CynthiaApiPoints,
+    ) => string
   >;
   modifyResponseHTMLBodyFragment: Array<
-      (htmlin: string,
-       metadata: ContentMetaDataType,
-       Cynthia: CynthiaApiPoints) => string
+    (
+      htmlin: string,
+      metadata: ContentMetaDataType,
+      Cynthia: CynthiaApiPoints,
+    ) => string
   >;
-  modifyRequest: Array<
-    (req: WebRequest,
-     Cynthia: CynthiaApiPoints) => void
-  >;
+  modifyRequest: Array<(req: WebRequest, Cynthia: CynthiaApiPoints) => void>;
   onLoad: Array<(Cynthia: CynthiaApiPoints) => void>;
   onClearInterval: Array<(Cynthia: CynthiaApiPoints) => void>;
 }
 export const newPluginBase: PluginBase = {
   modifyResponseHTML: [
-    (htmlin: string, _1,_2) => {
+    (htmlin: string, _1, _2) => {
       // Make no changes. Return unchanged.
       return htmlin;
     },
   ],
   modifyResponseHTMLBodyFragment: [
-    (htmlin: string, _1,_2) => {
+    (htmlin: string, _1, _2) => {
       // Return with a little comment.
       return `${htmlin}\n<!-- test... Body modifier Node plugins enabled! -->\n`;
     },
@@ -49,11 +53,7 @@ export const newPluginBase: PluginBase = {
     },
     (req: WebRequest, Cynthia: CynthiaApiPoints) => {
       req.get("/pltest*", () => {
-        const a: ResponderResponse = {
-          headers: [],
-          body: "This is a test response.",
-        };
-        return a;
+        return "This is a test response.";
       });
     },
   ],
